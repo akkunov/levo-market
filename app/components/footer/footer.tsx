d3ebb0cf6741f2edf2600a6aadfd7f099c3e0ebc
https://github.com/akkunov/print-viewmsroot <akunov313131@gmail.com>
2025-09-26 17:04:16 +0600

    asan usonov

--- app/components/footer/footer.tsx
+++ app/components/footer/footer.tsx
@@ -9,41 +9,59 @@ export const Footer: FC = () => {
     return (
         <footer className={`flex flex-col bg-[#101828] py-8`}>
             <Container>
-                <div className={`flex flex-row`}>
-                    <ul className={`text-sm text-white  grid grid-cols-1 max-w-1/2 p-2 gap-2 pb-8`}>
+                <div className={`flex flex-row px-4 gap-4`}>
+                    <ul className="text-sm text-white grid grid-cols-1 max-w-1/2 p-2 gap-2 pb-8">
                         <li>
-                            <Link href={`#`}
-                                  className={`inline-flex items-center gap-2 hover:underline hover:text-gray-300`}>Instagram <BsInstagram/></Link>
-                        </li>
-                        <li>
-                            <Link href={`#`}
-                                  className={`inline-flex items-center gap-2 hover:underline hover:text-gray-300`}>Telegram <SiTelegram/></Link>
-                        </li>
-                        <li>
-                            <Link href={`#`}
-                                  className={`inline-flex items-center gap-2 hover:underline hover:text-gray-300`}>WhatsApp <SiWhatsapp/>
+                            <Link
+                                href="#"
+                                className="inline-flex items-center gap-2 hover:underline hover:text-gray-300"
+                            >
+                                Instagram <BsInstagram />
                             </Link>
                         </li>
-                        <ul>
-                            <Link href={`mailto:akunov313131@gmail.com`}
-                                  className={`inline-flex items-center gap-2 hover:underline hover:text-gray-300`}>Levo@meraket.com </Link>
-                        </ul>
                         <li>
-                            <Link href={`tel:+996551069004`}
-                                  className={`inline-flex items-center gap-2 hover:underline hover:text-gray-300`}>По
-                                любым вопросам звоните: 0551 069 004 </Link>
+                            <Link
+                                href="#"
+                                className="inline-flex items-center gap-2 hover:underline hover:text-gray-300"
+                            >
+                                Telegram <SiTelegram />
+                            </Link>
+                        </li>
+                        <li>
+                            <Link
+                                href="#"
+                                className="inline-flex items-center gap-2 hover:underline hover:text-gray-300"
+                            >
+                                WhatsApp <SiWhatsapp />
+                            </Link>
+                        </li>
+                        <li>
+                            <Link
+                                href="mailto:akunov313131@gmail.com"
+                                className="inline-flex items-center gap-2 hover:underline hover:text-gray-300"
+                            >
+                                Levo@meraket.com
+                            </Link>
+                        </li>
+                        <li>
+                            <Link
+                                href="tel:+996551069004"
+                                className="inline-flex items-center gap-2 hover:underline hover:text-gray-300 whitespace-pre-wrap"
+                            >
+                                По любым вопросам звоните:
+                                0551 069 004
+                            </Link>
                         </li>
                     </ul>
-
-                    <li className={`text-sm text-white grid grid-cols-auto max-w-1/2 p-2 gap-2 pb-8`}>
+                    <ul className={`text-sm text-white grid grid-cols-auto max-w-1/2 p-2 gap-2 pb-8`}>
                         {mockCategories.map((items) => {
                             return (
-                                <ul key={items.id}>
+                                <li key={items.id}>
                                     <Link href="#" className={`hover:underline hover:text-gray-300`}>{items.name}</Link>
-                                </ul>
+                                </li>
                             )
                         })}
-                    </li>
+                    </ul>
                 </div>
             </Container>
             <div className={`h-[1px] bg-gray-400 w-full`}>
