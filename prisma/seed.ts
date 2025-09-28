5af7da2c18fe91c85da3b6aec57bab75a1743fec
https://github.com/akkunov/print-viewmsroot <akunov313131@gmail.com>
2025-09-28 18:44:20 +0600

    добавил все кроме детальнльглй страницы 6

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,21 +1,21 @@
-import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
-
-async function main() {
-    await prisma.product.deleteMany()
-    await prisma.category.deleteMany()
-
-    const cat1 = await prisma.category.create({ data: { name: 'Стиральные машины', slug: 'stiralnye-mashiny' } })
-    const cat2 = await prisma.category.create({ data: { name: 'Холодильники', slug: 'holodilniki' } })
-
-    await prisma.product.createMany({
-        data: [
-            { name: 'WashPro 300', price: 499.99, type: 'Стиральная машина', categoryId: cat1.id },
-            { name: 'CoolFridge X', price: 899.5, type: 'Холодильник', categoryId: cat2.id },
-        ],
-    })
-
-    console.log('Seed complete')
-}
-
-main().catch(e => { console.error(e); process.exit(1) }).finally(() => prisma.$disconnect())
+// import { PrismaClient } from '@prisma/client'
+// const prisma = new PrismaClient()
+//
+// async function main() {
+//     await prisma.product.deleteMany()
+//     await prisma.category.deleteMany()
+//
+//     const cat1 = await prisma.category.create({ data: { name: 'Стиральные машины', slug: 'stiralnye-mashiny' } })
+//     const cat2 = await prisma.category.create({ data: { name: 'Холодильники', slug: 'holodilniki' } })
+//
+//     await prisma.product.createMany({
+//         data: [
+//             { name: 'WashPro 300', price: 499.99, type: 'Стиральная машина', categoryId: cat1.id },
+//             { name: 'CoolFridge X', price: 899.5, type: 'Холодильник', categoryId: cat2.id },
+//         ],
+//     })
+//
+//     console.log('Seed complete')
+// }
+//
+// main().catch(e => { console.error(e); process.exit(1) }).finally(() => prisma.$disconnect())
