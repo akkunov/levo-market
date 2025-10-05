97b73ac2eafeec42016acbb5a709a8796839a3fd
Elpacho <akunov313131@gmail.com>
2025-10-05 15:13:56 +0600

    попытка деплоя 3

--- app/pages/CatalogPage.tsx
+++ app/pages/CatalogPage.tsx
@@ -133,7 +133,7 @@ export default function CatalogPage() {
                                     alt={p.title}
                                     className="w-full h-40 object-contain rounded-md"
                                 />
-                                <span className="mt-2 text-[12px] text-white/70">{p.catalog.name}</span>
+                                <span className="mt-2 text-[12px] text-black">{p.catalog.name}</span>
                                 <h2 className="font-semibold">{p.title}</h2>
                             </div>
                         ))}
