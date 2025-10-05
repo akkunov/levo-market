de7e70e60cb3412737ec0c8fc9c758837adeffd3
Elpacho <akunov313131@gmail.com>
2025-10-05 19:58:26 +0600

    Написал адаптивку

--- app/pages/CatalogPage.tsx
+++ app/pages/CatalogPage.tsx
@@ -126,7 +126,7 @@ export default function CatalogPage({slug}: {slug?:ParamValue}) {
                             </div>
                         ))
                         : products.map((p) => (
-                            <Link href={`${selectedSlug}/${p.id}`} key={p.id}>
+                            <Link href={`${selectedSlug}/${p.id}`} key={p.id} className={`p-2`}>
                                 <div key={p.id} className="border rounded-lg p-2 flex flex-col">
                                     <img
                                         src={p.image || ""}
