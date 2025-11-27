142e437687e6e9e865738f004c67998989807e74
msroot <akunov313131@gmail.com>
2025-11-27 10:13:32 +0600

    ::fix bug with load data with category

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -24,7 +24,7 @@ export async function GET(req: NextRequest) {
                         image: true,
                         price: true,
                     },
-                    orderBy: {id: "desc"}
+                    orderBy: {createdAt: "asc"}
                 }},
 
         })
